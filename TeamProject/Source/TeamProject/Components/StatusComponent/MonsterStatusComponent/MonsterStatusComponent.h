8d91ed4c78a56fbd53095e7d068477dabd8c7e80
assort_C2\assort_st2 <samang22@naver.com>
2025-05-14 17:59:34 +0900

    StatusComponent 추가

---
+++ TeamProject/Source/TeamProject/Components/StatusComponent/MonsterStatusComponent/MonsterStatusComponent.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StatusComponent/StatusComponent.h"
+#include "Misc/Utils.h"
+#include "MonsterStatusComponent.generated.h"
+
+UCLASS()
+class TEAMPROJECT_API UMonsterStatusComponent : public UStatusComponent
+{
+	GENERATED_BODY()
+	
+};
